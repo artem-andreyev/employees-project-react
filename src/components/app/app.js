5cf5b4d4fdedb117d85459b5a20260105fe7edf3
Artem <artem.andreyev212@gmail.com>
2024-06-23 17:48:50 +0300

    All modules added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,8 @@
 import AppInfo from "../app-info/app-info";
 import SeachPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
+import EmployeesList from "../employees-list/employees-list";
+import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
 import "./app.css";
 
@@ -13,6 +15,9 @@ function App() {
                 <SeachPanel></SeachPanel>
                 <AppFilter></AppFilter>
             </div>
+
+            <EmployeesList/>
+            <EmployeesAddForm/>
         </div>
     );
 }
