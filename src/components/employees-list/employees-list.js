dba914b7f151eddc847c328866a133b0e4fd2127
Artem <artem.andreyev212@gmail.com>
2024-06-29 20:27:46 +0300

    Working with my events for the removal of employees

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,12 +2,15 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import "./employees-list.css";
 
-const EmployeesList = ({data}) => {
+const EmployeesList = ({data, onDelete}) => {
 
     const elements = data.map(item => {
         const {id, ...itemProps} = item;
         return (
-            <EmployeesListItem key={id} {...itemProps}/> // {...item} (spread operator(разворачивает обьект на отдельные елементы)) идентичен name={item.name} salary={item.salary}
+            <EmployeesListItem 
+                key={id} 
+                {...itemProps} // {...item} (spread operator(expands the object into separate elements)) is identical to name={item.name} salary={item.salary})
+                onDelete={() => onDelete(id)}/>
         )
     })
 
