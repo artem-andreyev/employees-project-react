afb99bcac46e0bf65f52c9d4e168ccb28c7746ea
Artem <artem.andreyev212@gmail.com>
2024-07-06 15:34:18 +0300

    Downloaded scss, added some inline styles to work with them

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -2,20 +2,22 @@ import "./app-filter.css";
 
 const AppFilter = (props) => {
     const buttonsData = [
-        {name: "all", label: "All employees"},
-        {name: "rise", label: "Employees up for promotion"},
-        {name: "moreThen1000", label: "Salary is over $1,000"}
+        {name: "all", label: "All employees", colored: false},
+        {name: "rise", label: "Employees up for promotion", colored: false},
+        {name: "moreThen1000", label: "Salary is over $1,000", colored: false}
     ];
 
-    const buttons = buttonsData.map(({name, label}) => {
+    const buttons = buttonsData.map(({name, label, colored}) => {
         const active = props.filter === name; // instead of if (with true or false)
         const clazz = active ? "btn-light" : "btn-outline-light";
+        const style = colored ? {color: "red"} : null;
         return (
             <button
                 type="button"
                 className={`btn ${clazz}`}
                 key={name}
-                onClick={() => props.onFilterSelect(name)}>
+                onClick={() => props.onFilterSelect(name)}
+                style={style}>
                     {label}
             </button>
         )
